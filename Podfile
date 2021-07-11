2779d6240e0b9210b5a05889327c9ac4e1f50936
victorgonn@gmail.com <Vi207750>
2021-07-11 20:52:31 -0300

    adicionando Cocoa pods para gerenciamento de libs terceiras - Usar Alamofire e Promisse para gerenciar chamadas HTTP

---
+++ Podfile
@@ -0,0 +1,22 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'desafio' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for desafio
+  pod 'Alamofire', '~> 5.4'
+  pod 'AlamofireNetworkActivityLogger', '~> 3.4'
+  pod 'PromisesSwift'
+
+  target 'desafioTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'desafioUITests' do
+    # Pods for testing
+  end
+
+end
--- desafio/AppDelegate.swift
+++ desafio/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import AlamofireNetworkActivityLogger
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +16,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         print("Project runing on ", EnvironmentUtils.Enviroment, " configuration")
+        
+        //Alamofire debug logger
+        NetworkActivityLogger.shared.level = .debug
+        NetworkActivityLogger.shared.startLogging()
+        
         return true
     }
 
